8abf70282ba93cc808c48df293f5d00b5bea40f8
andi <andi@gmil.com>
2024-04-25 20:26:13 +0300

    LB3

---
+++ ComputerInfo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+public class Computer
+{
+    public string Name { get; set; }
+    public string CPU { get; set; }
+    public int RAM { get; set; }
+    public int Storage { get; set; }
+
+    public Computer(string name, string cpu, int ram, int storage)
+    {
+        Name = name;
+        CPU = cpu;
+        RAM = ram;
+        Storage = storage;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Computer Name: {Name}");
+        Console.WriteLine($"CPU: {CPU}");
+        Console.WriteLine($"RAM: {RAM}GB");
+        Console.WriteLine($"Storage: {Storage}GB");
+    }
+}
+
+public class ComputerInformationSystem
+{
+    public void GatherInformation(Computer computer)
+    {
+        Console.WriteLine("Gathering information about the computer...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Computer myComputer = new Computer("My PC", "Intel Core i7", 16, 512);
+        myComputer.DisplayInfo();
+
+        ComputerInformationSystem informationSystem = new ComputerInformationSystem();
+        informationSystem.GatherInformation(myComputer);
+
+        Console.ReadLine();
+    }
+}
